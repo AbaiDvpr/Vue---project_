33e701559c25b1d576dcecd82fe539b9dfc23f33
AbaiDvpr <abai.zeinolla.01@gmail.com>
2025-05-03 16:52:08 +0500

    new_ files

--- vue project/src/pages/Home/index.vue
+++ vue project/src/pages/Home/index.vue
@@ -1,3 +1,126 @@
 <template>
-    lll4
+   <div class="container-project">
+        <div class="container-show">
+            1
+        </div>
+        
+        <div class="container-template">
+            <div class="container-templateInner">
+                <p>
+                    Редактировать приглашение
+                </p>
+
+                <div>
+                    <p>
+                        Шаблоны
+                    </p>
+
+                    <ul>
+                        <li>
+                            <button>
+                                шаблон 1
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                шаблон 2
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                шаблон 3
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+
+                <input type="file">
+                
+                <div>
+                    <p>
+                        Заголовок 1
+                    </p>
+
+                    <input type="text" placeholder="Имя: Санжар">
+                </div>
+
+                <div>
+                    <p>
+                        Заголовок 2
+                    </p>
+
+                    <input type="text"  placeholder="Имя: Асем">
+                </div>
+
+
+                <div>
+                    <p>
+                        Заголовок 3
+                    </p>
+
+                    <input type="text"  placeholder="Имя: Асем">
+                </div>
+
+
+                <div>
+                    <p>
+                      Дата
+                    </p>
+
+                    <input type="text">
+                </div>
+
+                <div>
+                    <p>
+                      Время
+                    </p>
+
+                    <input type="text">
+                </div>
+
+
+                <div>
+                    <p>
+                     Р
+                    </p>
+
+                    <input type="text">
+                </div>
+
+
+                <div>
+                    <p>
+                     2gis
+                    </p>
+
+                    <input type="text">
+                </div>
+
+
+                <div>
+                    <p>
+                     2gis
+                    </p>
+
+                    <input type="text">
+                </div>
+
+
+
+
+                <div>
+                    <p>
+                     ---
+                    </p>
+
+                    <input type="text">
+                </div>
+
+
+            </div>
+        </div>
+   </div>
 </template>
+
+
+<style src="./main.css"></style>
